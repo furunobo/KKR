e125aa257f818140f4aa4fd8a9b12ba4812589a3
Noboru Furuya <furuya@g.ecc.u-tokyo.ac.jp>
2022-10-09 21:30:45 +0900

    set up physical_model

--- pty_wKKR3.py
+++ pty_wKKR3.py
@@ -13,7 +13,7 @@ intScanPix = 10
 intXScanNum = 15
 intYScanNum = 15
 
-intIterationNum = 200
+intIterationNum = 100
 fltAlpha = 0.5
 fltBeta = 0.5
 
@@ -31,38 +31,55 @@ img_int = np.array(img_int)
 filename = "fe_xmcd.csv"
 aryData = np.loadtxt(filename, delimiter=',', encoding='utf-8-sig')
 stop = 120
-step = 5
+step = 10
 energy = aryData[:stop:step, 0] # np.linspace(690, 710, 20)
 spcOrig = aryData[:stop:step, 1] # 1/((energy - 700)**2 + 2) 
 aryEnergy = np.repeat(energy, intObjectNum**2)
 aryEnergy = aryEnergy.reshape([len(energy), intObjectNum, intObjectNum])
 
-img_ph = np.empty((len(energy), intObjectNum, intObjectNum))
-img_tmp = np.empty((len(energy), intObjectNum, intObjectNum))
-for i in range(len(energy)):
-    img_int = img_int / np.max(img_int)
-    img_tmp[i] = np.exp(-spcOrig[i] * img_int)
-for k in range(intObjectNum):
-    for l in range(intObjectNum):
-        img_ph[:, k, l] = (ft.hilbert(np.log(img_tmp[:, k, l]) * aryEnergy[:, k, l] / 1.240)) * 1.240 / aryEnergy[:, k, l]
+def physical_model(object_image, energy_list): # 3D (energy, x, y) image to 3D image
+    hilbert_image = np.empty((len(energy_list), object_image.shape[1], object_image.shape[2]))
+    for k in range(object_image.shape[1]):
+        for l in range(object_image.shape[2]):
+            hilbert_image[:, k, l] = ft.hilbert(np.log(np.abs(object_image[:, k, l])) * energy_list[:] / 1.240)
+    return hilbert_image
 
-plt.plot(energy, img_ph[:, 150, 150])
+def generate_complex_image(image_int, energy_list, spectrum_exp): # 2D to 3D
+    image_tmp = np.empty((len(energy_list), image_int.shape[0], image_int.shape[1]))
+    for i in range(len(energy_list)):
+        image_tmp[i] = np.exp(-spectrum_exp[i] * image_int)
+    hil_img = physical_model(image_tmp, energy_list)
+    image_cx = np.empty_like(image_tmp) + 0j
+    for i in range(len(energy_list)):
+        image_cx[i] = image_tmp[i] * np.exp(1.0j * (hil_img[i] - 0.01) * 1.240 / energy_list[i])
+    return image_cx
+
+# img_ph = np.empty((len(energy), intObjectNum, intObjectNum))
+# img_tmp = np.empty((len(energy), intObjectNum, intObjectNum))
+# for i in range(len(energy)):
+#     img_int = img_int / np.max(img_int)
+#     img_tmp[i] = np.exp(-spcOrig[i] * img_int)
+# for k in range(intObjectNum):
+#     for l in range(intObjectNum):
+#         img_ph[:, k, l] = (ft.hilbert(np.log(img_tmp[:, k, l]) * aryEnergy[:, k, l] / 1.240)) * 1.240 / aryEnergy[:, k, l]
+
+img_int = img_int / np.max(img_int)
+# img_ph = generate_phase_image(img_int, energy, spcOrig)
+
+# plt.plot(energy, img_ph[:, 150, 150])
 # plt.imshow(np.abs(img_ph[0]))
 # plt.colorbar()
 # plt.title("Phase")
-plt.show()
-aryExpObject_sub = np.empty((len(energy), intObjectNum, intObjectNum), dtype = complex)
+# plt.show()
 
 listExpImg = []
+img = generate_complex_image(img_int, energy, spcOrig)
 for i in range(len(energy)):
 
-    img_int = img_int / np.max(img_int)
+    # img_int = img_int / np.max(img_int)
     # img_ph[i] = img_ph[i] / np.max(img_ph[i])
 
-    img = np.exp(-spcOrig[i] * img_int) * np.exp(1.0j * img_ph[i])
-
-    aryExpObject = img
-    aryExpObject_sub[i] = img
+    aryExpObject = img[i]
 
     y, x = np.indices((intProbePix, intProbePix))
 
@@ -105,9 +122,9 @@ plt.colorbar()
 plt.title("Phase.")
 plt.show()
 
-constKKR = ft.hilbert(np.log(np.abs(aryExpObject_sub[:, 150, 150])) * energy / 1.240) - (np.angle(aryExpObject_sub[:, 150, 150]) * energy / 1.240)
-plt.plot(energy, np.angle(aryExpObject_sub[:, 150, 150]), label = 'arg')
-plt.plot(energy, np.abs(aryExpObject_sub[:, 150, 150]), label = 'int')
+constKKR = ft.hilbert(np.log(np.abs(img[:, 150, 150])) * energy / 1.240) - (np.angle(img[:, 150, 150]) * energy / 1.240)
+plt.plot(energy, np.angle(img[:, 150, 150]), label = 'arg')
+plt.plot(energy, np.abs(img[:, 150, 150]), label = 'int')
 plt.plot(energy, np.exp(- spcOrig * 0.392), label = 'orig_int')
 plt.plot(energy, ft.hilbert((np.log(np.exp(- spcOrig * 0.392)) * energy / 1.240)) * 1.240 / energy, label = 'orig_arg')
 plt.plot(energy, constKKR, label = 'const')
@@ -168,15 +185,18 @@ for Iteration1 in range(intIterationNum):
             aryProbe[energyi] = np.roll(aryProbe[energyi], -np.argmax(np.sum(np.abs(aryProbe[energyi]), axis = 1)) + intProbePix // 2, axis = 0)
             aryProbe[energyi] = np.roll(aryProbe[energyi], -np.argmax(np.sum(np.abs(aryProbe[energyi]), axis = 0)) + intProbePix // 2, axis = 1)
 
-    if (Iteration1 + 1) % 100 == 0 and Iteration1 > 90:
-        spcPty = np.log(np.abs(aryObject)) * aryEnergy / 1.240
-        for l in range(intObjectNum):
-            for m in range(intObjectNum):
-                aryHilSpc[:, m, l] = ft.hilbert(spcPty[:, m, l])
-        constKKR = aryHilSpc - (np.angle(aryObject) * aryEnergy / 1.240)
+    if (Iteration1 + 1) % 100 == 0: #and Iteration1 > 90:
+        # spcPty = np.log(np.abs(aryObject)) * aryEnergy / 1.240
+        # for l in range(intObjectNum):
+        #     for m in range(intObjectNum):
+        #         aryHilSpc[:, m, l] = ft.hilbert(spcPty[:, m, l])
+        aryHilSpc = physical_model(np.abs(aryObject), energy)
+        constKKR = aryHilSpc - np.angle(aryObject) * aryEnergy / 1.240
         aveKKR = np.average(constKKR, axis=0)
 
         argObject = (aryHilSpc[:] - aveKKR) * 1.240 / aryEnergy[:]
+        plt.imshow(aveKKR)
+        plt.show()
         aryObject = np.sqrt(aryObject) * np.exp(1.0j * argObject)
         # plt.imshow(np.abs(aryObject[0]))
         # plt.show()
